f4802d3662f96d6ec701726cca4e2ba6564b461b
Gurnek Mokha <gmokha2@illinois.edu>
2019-02-23 11:14:44 -0600

    Set up tox and minimal tests (#21)
    * Set up the tox.ini file and added minimal tests to ensure it works.
    
    * Added basic tests for square strategy get_grid_arrangement function.

---
+++ tests/test_strategies.py
@@ -0,0 +1,45 @@
+import pytest
+
+from grid_strategy import strategies
+
+@pytest.fixture
+def rectangular_strategy():
+    return strategies.RectangularStrategy()
+
+@pytest.fixture
+def square_strategy():
+    return strategies.SquareStrategy()
+
+
+# Test the rectangular strategy to see if the get_grid_arrangement returns the right tuple.
+@pytest.mark.parametrize('num_plots, grid_arrangement', [
+    (1, (1, )),
+    (2, (2, )),
+    (3, (3, )),
+    (4, (2, 2)), 
+    (5, (5, )),
+    (6, (3, 3)),
+    (10, (5, 5)),
+    (12, (4, 4, 4)),
+    (20, (5, 5, 5, 5))
+])
+def test_rectangular_strategy(rectangular_strategy, num_plots, grid_arrangement):
+    assert rectangular_strategy.get_grid_arrangement(num_plots) == grid_arrangement
+
+
+@pytest.mark.parametrize('num_plots, grid_arrangement', [
+    (1, (1, )),
+    (2, (2, )),
+    (3, (2, 1)),
+    (4, (2, 2)),
+    (5, (2, 3)),
+    (6, (3, 3)),
+    (7, (2, 3, 2)), 
+    (8, (3, 2, 3)),
+    (9, (3, 3, 3)),
+    (10, (3, 4, 3)),
+    (12, (4, 4, 4)),
+    (20, (5, 5, 5, 5))
+])
+def test_square_strategy(square_strategy, num_plots, grid_arrangement):
+    assert square_strategy.get_grid_arrangement(num_plots) == grid_arrangement
